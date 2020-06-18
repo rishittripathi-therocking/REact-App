263e5c2aefa0c0b13d335c07015e894e3cb9dfb3
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-06-18 11:00:47 +0530

    react Componenet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menuComponent.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color="primary">
             <div className="container">
                 <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
             </div>
-        </Navbar>       
+        </Navbar> 
+        <Menu />      
     </div>
   );
 }
