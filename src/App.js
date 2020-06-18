27f3ecd8ecfc6feb74a9e366d213151d91267c8b
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-06-18 11:57:00 +0530

    react Componenet 2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/menuComponent.js';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-        <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
-            </div>
-        </Navbar> 
-        <Menu />      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes:DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+          <Navbar dark color="primary">
+              <div className="container">
+                  <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
+              </div>
+          </Navbar> 
+          <Menu dishes={this.state.dishes} />      
+      </div>
+    );
+  }
 }
 
 export default App;
